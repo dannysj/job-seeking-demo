3f6cad699d99fa3d20e5195adf09ebeea12b3020
Shawn Zhong <public@shawnzhong.com>
2018-05-29 23:09:54 +0800

    update db patches

--- server/db_patches/mentor_comment.sql
+++ server/db_patches/mentor_comment.sql
@@ -1,9 +1,17 @@
-create table if not exists mentor_comment(
-      id  serial not null
-          constraint mentor_comment_id_pk
-          primary key,
-      mid int references mentor_info(id),
-      author varchar(255) default '' :: character varying,
-      text   varchar(255) default '' :: character varying,
-      time_added   timestamp default now()
+create table if not exists mentor_comment
+(
+  id          serial  not null
+    constraint mentor_comment_id_pk
+    primary key,
+  mid         integer
+    constraint mentor_comment_mid_fkey
+    references mentor_info,
+  author      varchar(255) default '' :: character varying,
+  text        varchar(255) default '' :: character varying,
+  time_added  timestamp    default now(),
+  uid         integer not null
+    constraint mentor_comment_users_id_fk
+    references users,
+  profile_pic varchar(255)
 );
+
