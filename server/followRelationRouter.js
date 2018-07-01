da9e7972e8247c7da63aa08ee21ecb69c758faac
Junjie <xujunjiejack@gmail.com>
2018-07-01 17:05:59 +0800

    Further polish the follow function. Now the follower will receive the system notification from their mentor.

--- server/followRelationRouter.js
+++ server/followRelationRouter.js
@@ -35,8 +35,18 @@ const app = express.Router();
 //   });
 //
 
+app.post('/api/follow_user', (req, res) => {
+  db.createFollowerFolloweeRelationship(req.body.uid, req.body.followee_uid, (err) =>{
+    if (err) {
+      console.log(err);
+      res.json({code: 1, errMsg: 'Database Error'});
+      return;
+    }
+    res.json({code:0})
+  });
+})
 
-// API: unfollow_user. post: body:{follower_uid:, followee_uid:}
+// API: unfollow_user. post: body:{uid:, followee_uid:}
 // call db to delete followRelationship in a table.
 app.post('/api/unfollow_user', (req, res) => {
   db.deleteFollowerFolloweeRelationship(req.body.uid, req.body.followee_uid, (err) => {
@@ -50,8 +60,8 @@ app.post('/api/unfollow_user', (req, res) => {
 })
 
 app.post('/api/whether_followed', (req, res) => {
-  // Request body. req.body{follower_uid, followee_uid"}
-  db.whetherFollowed(req.body.follower_uid, req.body.followee_uid, (err, whetherFollowed) => {
+  // Request body. req.body{uid, followee_uid"}
+  db.whetherFollowed(req.body.uid, req.body.followee_uid, (err, whetherFollowed) => {
       if (err) {
         console.log(err);
         res.json({code: 1, errMsg: 'Database Error'});
--- server/mentorMenteeRouter.js
+++ server/mentorMenteeRouter.js
@@ -67,30 +67,8 @@ app.post('/api/mentee_confirm', (req, res) => {
 });
 
 
-// API follow_user. post: body:{follower_uid:, followee_uid:}
-app.post('/api/follow_user', (req, res)=>{
-  db.createFollowerFolloweeRelationship(req.body.uid, req.body.followee_uid, (err)=>{
-      if (err) {
-        console.log(err);
-        res.json({code: 1, errMsg: 'Database Error'});
-        return;
-      }
-      res.json({code:0})
-  });
-})
-
 // API: unfollow_user. post: body:{follower_uid:, followee_uid:}
 // call db to delete followRelationship in a table.
-app.post('/api/unfollow_user', (req, res)=>{
-  db.deleteFollowerFolloweeRelationship(req.body.uid, req.body.followee_uid, (err)=>{
-      if (err) {
-        console.log(err);
-        res.json({code: 1, errMsg: 'Database Error'});
-        return;
-      }
-      res.json({code:0})
-  });
-})
 
 app.post('/api/whether_followed', (req, res) => {
   // Request body. req.body{follower_uid, followee_uid"}
