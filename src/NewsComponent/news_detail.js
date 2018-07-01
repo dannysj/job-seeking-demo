230b7cea857566808bff6dd2e4231beff5a32a2d
Shawn Zhong <public@shawnzhong.com>
2018-07-02 02:58:39 +0800

    Minor

--- src/NewsComponent/news_detail.js
+++ src/NewsComponent/news_detail.js
@@ -27,8 +27,10 @@ class NewsDetail extends Component {
       if(res.data.code===0){
         let curState = this.state;
         curState.news = res.data.news;
-        if (curState.news.content === null) // compatibility, will be removed and use delta
+
+        if (curState.news.delta["ops"] !== null) // compatibility, will be removed and use delta
           curState.news.content = new QuillDeltaToHtmlConverter(curState.news.delta["ops"]).convert();
+        
         curState.loading = false;
         this.setState(curState);
       }
