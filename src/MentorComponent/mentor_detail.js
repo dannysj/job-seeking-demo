e8df4b3b5c215b076bb1cae1afbd01b353d43a49
dannysj <csji4202@hotmail.com>
2018-07-02 11:01:44 -0500

    USD

--- src/MentorComponent/mentor_detail.js
+++ src/MentorComponent/mentor_detail.js
@@ -360,7 +360,7 @@ class MentorDetail extends Component {
                     <div className="service-item" >
                       <div className="service-title">
                         <div className="service-name">{el.name}</div>
-                        <div className="service-price">{el.price+' RMB'}</div>
+                        <div className="service-price">{el.price+' USD'}</div>
                       </div>
                       <p className="service-description">{el.description} </p>
                       <div className="buy-button"><Button className="buy-button-ani" onClick={()=>this.initBuy(el.name, el.price)}>购买</Button></div>
