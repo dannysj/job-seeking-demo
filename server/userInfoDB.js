be46bcf98ea4bdf363537bcfa3f1b4083c5092c7
Shawn Zhong <public@shawnzhong.com>
2018-07-06 19:49:00 +0800

    Update user_info api to include followee column

--- server/userInfoDB.js
+++ server/userInfoDB.js
@@ -1,48 +1,40 @@
 const db = require('./_dbPool.js');
 
+const selectClause = `
+  select
+    users.*,
+    (select count(*)::int from message where is_read=false and destination = users.id) as num_notifications,
+    array(select follow_rel.followee_uid from follow_rel where follow_rel.follower_uid = users.id) as followee
+  from users
+  `;
+
+const userInfoCallBack = callback => (err, result) => {
+  if (err) {
+    callback(err);
+    return;
+  }
+
+  if (result.rows.length === 0) {
+    callback('No such email found');
+    return;
+  }
+
+  let userAccount = result.rows[0];
+  delete userAccount.password;
+  callback(null, userAccount);
+};
+
 exports.getUserInfo = (uid, callback) => {
-  const query = `select * from users where users.id = $1`;
-  db.query(query, [uid], (err, result) => {
-    if (err) {
-      callback(err);
-      return;
-    }
+  const query = selectClause + `where users.id = $1;`;
+  db.query(query, [uid], userInfoCallBack(callback));
+};
 
-    if (result.rows.length === 0) {
-      callback('No such email found');
-      return;
-    }
-
-    let userAccount = result.rows[0];
-    userAccount.password = null;
-    db.query(`select count(*) as count from message
-              where is_read=false and destination=$1`, [uid])
-      .then(result => {
-        userAccount.num_notifications = parseInt(result.rows[0].count);
-        callback(null, userAccount);
-      }).catch(err => callback(err))
-  });
+exports.verifyUser = (user, callback) => {
+  const query = selectClause + `where email=$1 and password=$2;`;
+  db.query(query, [user.email, user.password], userInfoCallBack(callback));
 };
 
 exports.updateUser = (data, callback) => {
   const query = `update users set ` + data.attr + `=$1 where id=$2;`;
   db.query(query, [data.val, data.uid]).then(() => callback(null)).catch(e => callback(e));
-};
-
-exports.verifyUser = function (user, callback) {
-  console.log(user.email);
-  const query = `select * from users where email=$1 and password=$2;`;
-  db.query(query, [user.email, user.password], (err, result) => {
-    if (err) {
-      callback(err);
-      return;
-    }
-    if (result.rows.length === 0) {
-      callback('No such email found');
-      return;
-    }
-    var userAccount = result.rows[0];
-    delete userAccount.password;
-    callback(null, userAccount);
-  });
 };
