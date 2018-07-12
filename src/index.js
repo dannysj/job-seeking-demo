afd7e33850f2ae4689cdd36b96f10d6b51896a7c
Shawn Zhong <public@shawnzhong.com>
2018-07-12 15:17:01 +0800

    Set default axios baseURL
     axios.defaults.baseURL = protocol + '//' + hostname + ':' + port;

--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import './index.less';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
 // Redux
 import {Provider} from "react-redux"
 import store from "./redux";
-
 // axios setting
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_HOST;
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const {protocol, hostname} = window.location;
+  const port = 3005;
+  axios.defaults.baseURL = protocol + '//' + hostname + ':' + port;
+}
+
 
 const user = store.getState().user;
-if(user && user.access_token)
+if (user && user.access_token)
   axios.defaults.headers.common['access_token'] = user.access_token;
 
 ReactDOM.render(
