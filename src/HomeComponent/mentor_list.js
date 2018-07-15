1cf9b6ed61e79f219c51a840655709a4626b637a
dannysj <csji4202@hotmail.com>
2018-07-15 00:24:32 -0500

    Added loading bar for mentor list

--- src/HomeComponent/mentor_list.js
+++ src/HomeComponent/mentor_list.js
@@ -7,9 +7,26 @@ import {connect} from 'react-redux'
 
 class MentorList extends Component {
 
+  constructor (props) {
+    super(props);
+    this.state = {
+      loaded: [],
+    }
+
+    this.handleImageLoaded = this.handleImageLoaded.bind(this);
+  }
+
   componentWillMount(){
     store.dispatch(fetchMentorList());
   }
+
+  handleImageLoaded(index) {
+    if (!this.state.loaded[index]) {
+        let curState = this.state;
+        curState.loaded[index] = true;
+        this.setState({ curState });
+    }
+}
 
   render() {
 
@@ -18,7 +35,8 @@ class MentorList extends Component {
         {this.props.mentors.slice(0,8).map((el, index) => (
           <Link to={'/mentor/'+el.mid} key={index}>
             <div className="mentor-overview_container" key={el.id}>
-              <img className="mentor-overview-picture" src={el.profile_pic} alt={el.last + '' + el.first}/>
+              <img className={"mentor-overview-picture" + (this.state.loaded[index] ? "" : " on-load")} src={el.profile_pic} alt={el.last + '' + el.first} onLoad={() => {
+                this.handleImageLoaded(index)}}/>
               <div className="mentor-overview-placeholder">
                 <h4>{el.last+' '}{el.first}</h4>
                 <p className="mentor-overview-text">{el.offer_company+' '+el.offer_title}</p>
